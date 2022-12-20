d621d45bb1001e3dcedd51df6b386931040c5fac
RodrigoProject <jairoRodrigoMP@gmail.com>
2022-12-20 11:54:34 -0500

    CRUD_BASICO

--- js/index.js
+++ js/index.js
@@ -17,27 +17,34 @@ let userAdmin = {
     nombre:'',
     apellido: '',
     edad:0,
-    fecha:''
+    profesion:'',
+    fecha:'', 
+    fecha_actu:''
 }
 
 // array para recbir objeto
 let lista =[];
+
+let eliminar = false;
 
 
 function insertarEmpleado(){
     let nombreV = prompt("Escribe tu nombre");
     let apellidoV = prompt("Escribe tu apellido");
     let edadV = parseInt(prompt("Escribe tu edad"));
+    let profesionV = prompt("Escribe la profesion");
 
     if(nombreV=='' || apellidoV==''||edadV==''){
         alert("todos los campos rellena");
         return;
     }
    
+    userAdmin.profesion=profesionV;
     userAdmin.nombre = nombreV;
     userAdmin.apellido = apellidoV;
     userAdmin.edad = edadV;
-    userAdmin.id = lista.length+1;
+    userAdmin.id++;
+    userAdmin.fecha_actu='';
 
     // Generando la fecha con segundos
     const tiempoTranscurrido = Date.now();
@@ -45,6 +52,7 @@ function insertarEmpleado(){
     userAdmin.fecha = hoy.toUTCString();
     crearEmpleado();
 }
+
 
 function crearEmpleado(){
     lista.push({...userAdmin});
@@ -57,12 +65,12 @@ function crearEmpleado(){
 
   function ordenarPorAtributo(atributo) {
   
-    let validInput = ["id", "nombre", "apellido", "edad", "fecha", "fecha actualizada"]
+    let validInput = ["id", "nombre", "apellido", "edad","profesion", "fecha", "fecha actualizada"]
   
     if (validInput.includes(atributo)) {
-      if (atributo === "edad" || atributo === "id" || atributo =="fecha") {
+      if (atributo === "edad" || atributo === "id") {
         lista.sort((a, b) => a[atributo] - b[atributo])
-      } else {
+      } else if(atributo=="profesion" || atributo=="nombre" || atributo=="apellido"){
         lista.sort((a, b) => a[atributo].localeCompare(b[atributo]));
       }
     } else {
@@ -72,7 +80,7 @@ function crearEmpleado(){
   
   
   //mostrar encabezado
-  const columnas = ["id", "nombre", "apellido", "edad", "fecha", "fecha actualizada"]
+  const columnas = ["id", "nombre", "apellido", "edad","profesion", "fecha", "fecha actualizada"]
   const table = document.createElement("table")
   table.classList.add("table", "table-bordered")
   
@@ -118,20 +126,76 @@ function crearEmpleado(){
   // Eliminar Empleado
 function eliminarData(){
 
+    eliminar = true;
     let id = parseInt(prompt("Ingrese el id a eliminar"));
 
     lista = lista.filter(data=>data.id !== id);
 
+
     mostrarUsuariosTabla(lista);
+    return eliminar;
 }
   
+
+
+// Editar
+function modificarRegistro() {
+    // Pedir al usuario que ingrese el ID del registro que desea modificar
+    let id = prompt("Ingrese el ID del registro que desea modificar:");
+  
+    // Buscar el registro con el ID especificado en el array de usuarios
+    let registro = lista.find(users=> users.id == id);
+  
+    // Verificar si se encontró el registro
+    if (!registro) {
+      // Si el registro no se encontró, mostrar un mensaje de error
+      //alert("No se encontró ningún registro con el ID especificado.");
+      window.close();
+      return;
+    }
+  
+    // Mostrar un promts para modificar el registro
+    let nombre = prompt("Ingrese el nuevo nombre para el usuario:");
+    let apellido = prompt("Ingrese el nuevo apellido para el usuario:");
+    let edad = prompt("Ingrese la nueva edad para el usuario:");
+    let profesion = prompt("Ingrese la nueva profesión para el usuario:");
+
+  
+    // Actualizar el registro en el array con los nuevos valores proporcionados por el usuario
+    const tiempoTranscurrido = Date.now();
+    const hoy = new Date(tiempoTranscurrido);
+
+    registro.nombre = nombre;
+    registro.apellido = apellido;
+    registro.edad = edad;
+    registro.profesion = profesion;
+
+    // Generando la fecha con segundos
+    registro.fecha_actu = hoy.toUTCString();
+
+
+    
+    mostrarUsuariosTabla(lista);
+    
+  }
+
+
+function actualizar(){
+
+
+    lista = registro.filter(data=>data.id == id);
+
+    modificarRegistro(lista);
+    
+}
+
  
 //botones
 const btnAñadir = document.createElement("button")
 btnAñadir.classList.add("btn", "btn-primary")
 btnAñadir.textContent = "Agregar usuario"
 btnAñadir.onclick=()=>{
-    if(lista.length>1){
+    if(lista.length>0){
         btnEliminar.disabled=false;
     }
     insertarEmpleado();
@@ -140,6 +204,9 @@ btnAñadir.onclick=()=>{
 const btnEditar = document.createElement("button")
 btnEditar.classList.add("btn", "btn-warning", "ms-2")
 btnEditar.textContent = "Editar usuario"
+btnEditar.onclick=function(){
+    modificarRegistro();
+}
 
 const btnEliminar = document.createElement("button");
 btnEliminar.classList.add("btn", "btn-danger", "ms-2");
@@ -148,6 +215,8 @@ btnEliminar.disabled=true;
 btnEliminar.onclick=()=>{
     eliminarData();
 }
+
+
 
 
 
