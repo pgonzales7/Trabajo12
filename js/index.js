ce51838a63d0373bf339bf2c56729782bc4b87b2
RodrigoProject <jairoRodrigoMP@gmail.com>
2022-12-20 10:53:10 -0500

    Arreglando el id

--- js/index.js
+++ js/index.js
@@ -7,19 +7,7 @@
 
 // const Create 
 const root = document.getElementById("root");
-const h2 = document.createElement('h2');
-h2.innerHTML="Create";
-root.append(h2);
 
-const button = document.createElement("button");
-button.innerHTML="Validar";
-root.append(button);
-
-
-const btnEliminar = document.createElement("button");
-btnEliminar.innerHTML="Eliminar";
-btnEliminar.disabled=true;
-root.append(btnEliminar);
 
 // El admin debe poder crear un nuevo registro de usuario utilizando la funcion 10. (Pueden utilizar un boton que diga "Crear Usuario")
 
@@ -35,36 +23,6 @@ let userAdmin = {
 // array para recbir objeto
 let lista =[];
 
-// const generarId=()=>{
-//     const random = Math.random().toString(32).substring(2);
-
-//     const fecha = Date.now().toString(32);
-//     return random+fecha;
-// }
-
-
-button.onclick=function(e){
-    e.preventDefault();
-    
-    if(lista.length>1){
-        btnEliminar.disabled=false;
-    }
-    insertarEmpleado();
-}
-
-btnEliminar.onclick=function(e){
-    e.preventDefault();
-    eliminarData();
-}
-
-
-
-function crearEmpleado(){
-    lista.push({...userAdmin});
-
-    
-    return lista;
-}
 
 function insertarEmpleado(){
     let nombreV = prompt("Escribe tu nombre");
@@ -75,31 +33,125 @@ function insertarEmpleado(){
         alert("todos los campos rellena");
         return;
     }
-
-    // userAdmin.id = generarId();    
-    userAdmin.id = lista.length+0;
+   
     userAdmin.nombre = nombreV;
     userAdmin.apellido = apellidoV;
     userAdmin.edad = edadV;
+    userAdmin.id = lista.length+1;
 
     // Generando la fecha con segundos
     const tiempoTranscurrido = Date.now();
     const hoy = new Date(tiempoTranscurrido);
     userAdmin.fecha = hoy.toUTCString();
-
-    console.log(generarId);
     crearEmpleado();
+}
+
+function crearEmpleado(){
+    lista.push({...userAdmin});
+    mostrarUsuariosTabla(lista)
 }
 
 
 
-// Eliminar
-// Eliminar Empleado
+//READ
+
+  function ordenarPorAtributo(atributo) {
+  
+    let validInput = ["id", "nombre", "apellido", "edad", "fecha", "fecha actualizada"]
+  
+    if (validInput.includes(atributo)) {
+      if (atributo === "edad" || atributo === "id" || atributo =="fecha") {
+        lista.sort((a, b) => a[atributo] - b[atributo])
+      } else {
+        lista.sort((a, b) => a[atributo].localeCompare(b[atributo]));
+      }
+    } else {
+      console.log("Debes ingresar un atributo válido");
+    }
+  }
+  
+  
+  //mostrar encabezado
+  const columnas = ["id", "nombre", "apellido", "edad", "fecha", "fecha actualizada"]
+  const table = document.createElement("table")
+  table.classList.add("table", "table-bordered")
+  
+  const thead = document.createElement("thead")
+  const trHead = document.createElement("tr")
+  
+  columnas.forEach(columna => {
+    const th = document.createElement("th")
+    th.textContent = columna
+    th.addEventListener("click", (e) => {
+      ordenarPorAtributo(e.target.textContent);
+      table.innerHTML = ""
+      table.appendChild(thead)
+      mostrarUsuariosTabla(lista)
+    })
+    trHead.appendChild(th)
+  })
+  thead.appendChild(trHead)
+  table.appendChild(thead)
+  root.appendChild(table)
+  
+  
+  //funcion mostrar los usuarios en la tabla
+  function mostrarUsuariosTabla(lista) {
+    table.innerHTML = ""
+    table.append(thead)
+    for (const index in lista) {
+      const tr = document.createElement("tr")
+      for (const key in lista[index]) {
+        const td = document.createElement("td")
+        td.textContent = lista[index][key]
+        tr.append(td)
+      }
+      table.append(tr)
+    }
+    console.log(lista)
+  }
+  mostrarUsuariosTabla(lista)
+  
+  
+
+  // eliminar data
+  // Eliminar Empleado
 function eliminarData(){
 
     let id = parseInt(prompt("Ingrese el id a eliminar"));
 
     lista = lista.filter(data=>data.id !== id);
 
-    console.log(lista);
+    mostrarUsuariosTabla(lista);
 }
+  
+ 
+//botones
+const btnAñadir = document.createElement("button")
+btnAñadir.classList.add("btn", "btn-primary")
+btnAñadir.textContent = "Agregar usuario"
+btnAñadir.onclick=()=>{
+    if(lista.length>1){
+        btnEliminar.disabled=false;
+    }
+    insertarEmpleado();
+}
+
+const btnEditar = document.createElement("button")
+btnEditar.classList.add("btn", "btn-warning", "ms-2")
+btnEditar.textContent = "Editar usuario"
+
+const btnEliminar = document.createElement("button");
+btnEliminar.classList.add("btn", "btn-danger", "ms-2");
+btnEliminar.textContent = "Eliminar usuario";
+btnEliminar.disabled=true;
+btnEliminar.onclick=()=>{
+    eliminarData();
+}
+
+
+
+const botones = document.getElementById("botones")
+botones.appendChild(btnAñadir)
+botones.appendChild(btnEditar)
+botones.appendChild(btnEliminar)
